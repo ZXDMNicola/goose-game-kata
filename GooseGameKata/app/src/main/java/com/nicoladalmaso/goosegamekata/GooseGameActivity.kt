8b36141c49e6f58163ffdddfad30dee9e05aac93
Nicola Dal Maso <nicola.dalmaso@zextras.com>
2023-08-03 11:55:17 +0200

    chore: Activity setup

--- GooseGameKata/app/src/main/java/com/nicoladalmaso/goosegamekata/GooseGameActivity.kt
+++ GooseGameKata/app/src/main/java/com/nicoladalmaso/goosegamekata/GooseGameActivity.kt
@@ -1,15 +1,18 @@
 package com.nicoladalmaso.goosegamekata
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import com.nicoladalmaso.goosegamekata.databinding.ActivityGooseGameBinding
 
 class GooseGameActivity : AppCompatActivity() {
 
     private val viewModel: GooseGameActivityViewModel by viewModels()
 
+    private lateinit var binding: ActivityGooseGameBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_goose_game)
     }
 }
